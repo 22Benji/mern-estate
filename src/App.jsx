96e7e8e1421204d9ea737536dd5892ff037f277d
Biniam Getu <biniam@Biniams-iMac.local>
2024-01-15 18:45:15 +0300

    create pages & route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Home from './pages/Home';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import React from 'react';
 
 export default function App() {
   return (
-    <h1 className=' text-red-500'>Benony Here He Comes</h1>
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      {/* <Route path="*" element={<p>Path not resolved</p>} /> */}
+      <Route path='/sign-in' element={<SignIn/>} />
+      <Route path='/sign-up' element={<SignUp />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/profile' element={<Profile />} />
+    </Routes>
+  </BrowserRouter>
   )
 }
